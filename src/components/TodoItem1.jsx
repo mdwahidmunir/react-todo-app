02d71e830e8d622525af03146e2002b3bc9bb164
mdwahidmunir <mdwahid.munir1@gmail.com>
2023-12-31 11:47:56 +0530

    Added variables

--- src/components/TodoItem1.jsx
+++ src/components/TodoItem1.jsx
@@ -1,9 +1,11 @@
 const TodoItem1 = () => {
+  const todo = "Buy Milk 2";
+  const date = "28/12/2023";
   return (
     <div class="container">
       <div class="row kg-row">
-        <div class="col-6">Buy Milk</div>
-        <div class="col-4">28/12/2023</div>
+        <div class="col-6">{todo}</div>
+        <div class="col-4">{date}</div>
         <div class="col-2 btn btn-danger kg-button">Remove</div>
       </div>
     </div>
