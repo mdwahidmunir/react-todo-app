472898582e1d35314929cdaa566d6cf79eb89b23
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-01-13 21:33:57 +0530

    Added useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import Welcome from "./components/Welcome";
 import { TodoItemsContext } from "./store/todoItemsStore";
 
@@ -22,8 +22,33 @@ const INITIAL_ITEMS = [
   // },
 ];
 
+function todoItmesReducer(currentTodoItems, action) {
+  /*     state
+  {
+    id: str,
+    name : str,
+    dueDate : str
+  }
+  */
+
+  let newTodoItems = currentTodoItems;
+  if (action.type === "ADD_TODO_ITEMS") {
+    const newItem = action.payload;
+    newTodoItems = [...currentTodoItems, newItem];
+  } else if (action.type === "DELETE_TODO_ITEMS") {
+    newTodoItems = currentTodoItems.filter(
+      (item) => item.id !== action.payload.item_id
+    );
+  }
+  return newTodoItems;
+}
+
 function App() {
-  const [items, setItems] = useState(INITIAL_ITEMS);
+  // const [items, setItems] = useState(INITIAL_ITEMS);
+  const [items, dispatchTodoItems] = useReducer(
+    todoItmesReducer,
+    INITIAL_ITEMS
+  );
 
   const addItem = (item) => {
     // Method 1
@@ -31,10 +56,17 @@ function App() {
     // setItems(newItems);
 
     // Method 2 NOTE: This is recommended when your state depends on the previous state
-    setItems((prevState) => {
-      const newItems = [item, ...prevState];
-      return newItems;
-    });
+    // setItems((prevState) => {
+    //   const newItems = [item, ...prevState];
+    //   return newItems;
+    // });
+
+    // Method 3 useReducer
+    const newItemAction = {
+      type: "ADD_TODO_ITEMS",
+      payload: item,
+    };
+    dispatchTodoItems(newItemAction);
   };
 
   const removeItem = (item_id) => {
@@ -43,10 +75,17 @@ function App() {
     // setItems(newItems);
 
     // Method 2
-    setItems((prevState) => {
-      const newItems = items.filter((item) => item.id !== item_id);
-      return newItems;
-    });
+    // setItems((prevState) => {
+    //   const newItems = items.filter((item) => item.id !== item_id);
+    //   return newItems;
+    // });
+
+    // Method 3
+    const deleteItemAction = {
+      type: "DELETE_TODO_ITEMS",
+      payload: { item_id: item_id },
+    };
+    dispatchTodoItems(deleteItemAction);
   };
 
   return (
