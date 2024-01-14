f404ed92fa248da702ce30b6a4218c494200d1d8
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-01-14 19:07:01 +0530

    Refactored use Reducer and leaned down App component

--- src/store/todoItemsStore.jsx
+++ src/store/todoItemsStore.jsx
@@ -1,7 +1,79 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItemsContext = createContext({
   todoItems: [],
   addItems: () => {},
   deleteItems: () => {},
 });
+
+function todoItemsReducer(currentTodoItems, action) {
+  /*     state
+  {
+    id: str,
+    name : str,
+    dueDate : str
+  }
+  */
+  let newTodoItems = currentTodoItems;
+  if (action.type === "ADD_TODO_ITEMS") {
+    const newItem = action.payload;
+    newTodoItems = [...currentTodoItems, newItem];
+  } else if (action.type === "DELETE_TODO_ITEMS") {
+    newTodoItems = currentTodoItems.filter(
+      (item) => item.id !== action.payload.item_id
+    );
+  }
+  return newTodoItems;
+}
+
+const INITIAL_ITEMS = [
+  // {
+  //   name: "Buy Milk",
+  //   dueDate: "12/12/2023",
+  // },
+  // {
+  //   name: "Study React",
+  //   dueDate: "23/12/2023",
+  // },
+  // {
+  //   name: "DSA Prep",
+  //   dueDate: "31/12/2023",
+  // },
+];
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [items, dispatchTodoItems] = useReducer(
+    todoItemsReducer,
+    INITIAL_ITEMS
+  );
+
+  const addItem = (item) => {
+    const newItemAction = {
+      type: "ADD_TODO_ITEMS",
+      payload: item,
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  const removeItem = (item_id) => {
+    const deleteItemAction = {
+      type: "DELETE_TODO_ITEMS",
+      payload: { item_id: item_id },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems: items,
+        addItems: addItem,
+        deleteItems: removeItem,
+      }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
